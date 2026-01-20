10394b0b335f87926ac772d0e56bc68618abe4e8
Karam G <karamgajbhiye@gmail.com>
2026-01-20 20:17:19 +0530

    Time: 555 ms (7.55%), Space: 0B (100%) - LeetHub

---
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -0,0 +1,16 @@
+/* Write your PL/SQL query statement below */
+
+with query AS (
+    
+select class,count(student) from courses
+group by class
+having count(student)>=5
+
+)
+select class from query
+
+-- select class from (
+-- select class,count(student) from courses
+-- group by class
+-- having count(student)>=5
+-- ) A
