eaa635cd56411d24c8cbc31bee37f33e7e2bb326
Karam G <karamgajbhiye@gmail.com>
2026-02-03 20:42:01 +0530

    Time: 859 ms (13.46%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -5,6 +5,6 @@ select product_id,year,quantity,price,
 rank() over(partition by product_id  order by year) rk from sales
 )
 select product_id,year as first_year,quantity,price from Results
-where rk=1;
+where rk=1 ;
 
 
